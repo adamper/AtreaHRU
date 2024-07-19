912cd3119ae320f767bd7bf926738ceaa3903dcb
adamp <admin.adamp@gmail.com>
2024-07-19 08:32:20 +0200

    Fixed issue with different types of GetOnState

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,30 +1,20 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Service, PlatformAccessory } from 'homebridge';
-
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { HRUPlatform } from './platform';
+import ModbusRTU from 'modbus-serial';
 
-const ModbusRTU = require('modbus-serial');
-const client = new ModbusRTU();
-
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
 export class HRUAccessory {
   private service: Service;
+  private client: ModbusRTU;
+  private isConnected: boolean = false;
 
   constructor(
     private readonly platform: HRUPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
-
+    this.client = new ModbusRTU();
     this.connectModbusClient();
 
     this.service = this.accessory.getService(this.platform.Service.Fan) || this.accessory.addService(this.platform.Service.Fan);
-    //this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
 
     this.service.getCharacteristic(this.platform.Characteristic.On)
       .onGet(this.handleOnGetOn.bind(this))
@@ -33,99 +23,106 @@ export class HRUAccessory {
     this.service.getCharacteristic(this.platform.Characteristic.RotationSpeed)
       .onGet(this.handleOnGetSpeed.bind(this))
       .onSet(this.handleOnSetSpeed.bind(this));
-
-
-
   }
 
-  async handleOnGetOn() {
-    let currentValue;
+  private async connectModbusClient() {
     try {
-      const response = await client.readInputRegisters(this.platform.regimeRegister, 1);
-      currentValue = response.data[0];
-      if (currentValue >= 2) {
-        currentValue = 1;
-      } else if (currentValue === 0) {
-        //keep currentValue 0
-      }
-
-
-    //   return 1;
+      await this.client.connectTCP(this.platform.ip, { port: this.platform.port });
+      this.isConnected = true;
+      this.platform.log.info(`Connected to Modbus device at ${this.platform.ip}:${this.platform.port}`);
     } catch (error) {
-      console.error(error);
-    }
-
-    // set this to a valid value for On
-    this.platform.log.debug('State is:', currentValue);
-    //currentValue = 1;
-
-    return currentValue;
-  }
-
-  /**
-   * Handle requests to set the "On" characteristic
-   */
-  async handleOnSetOn(value) {
-    try{
-      if (value >= 1) {
-        value = 2;
-      } else if (value === 0) {
-        //keep currentValue 0
+      this.isConnected = false;
+      if (error instanceof Error) {
+        this.platform.log.error(`Error connecting to Modbus device: ${error.message}`);
+      } else {
+        this.platform.log.error('An unknown error connecting to Modbus device.');
       }
-      await client.writeRegisters(this.platform.regimeRegister, [value]);
-    }catch (error) {
-      console.error(error);
+      setTimeout(() => this.connectModbusClient(), 5000); // Try to reconnect after 5 seconds
     }
   }
 
-  async handleOnGetSpeed() {
-    let currentValue;
+  private async ensureConnection() {
+    if (!this.isConnected) {
+      await this.connectModbusClient();
+    }
+  }
+
+  async handleOnGetOn(): Promise<CharacteristicValue> {
     try {
-      const response = await client.readInputRegisters(this.platform.speedRegister, 1);
-      currentValue = response.data[0];
-
-
-    //   return 1;
+      await this.ensureConnection();
+      const response = await this.client.readInputRegisters(this.platform.regimeRegister, 1);
+      let currentValue = response.data[0];
+      currentValue = currentValue >= 2 ? 1 : currentValue;
+      this.platform.log.debug('State is:', currentValue);
+      return currentValue;
     } catch (error) {
-      console.error(error);
+      if (error instanceof Error) {
+        this.platform.log.error(`Error getting On state: ${error.message}`);
+      } else {
+        this.platform.log.error('An unknown error occurred while getting On state');
+      }
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
-
-    // set this to a valid value for On
-    this.platform.log.debug('Speed is:', currentValue);
-    //currentValue = 1;
-
-    return currentValue;
   }
 
-  /**
-   * Handle requests to set the "On" characteristic
-   */
-  async handleOnSetSpeed(value) {
-    try{
-      const response = await client.readInputRegisters(this.platform.regimeRegister, 1);
+  async handleOnSetOn(value: CharacteristicValue) {
+    try {
+      await this.ensureConnection();
+      let setValue = value as number;
+      if (setValue >= 1) {
+        setValue = 2;
+      }
+      await this.client.writeRegisters(this.platform.regimeRegister, [setValue]);
+      this.platform.log.debug(`Set On state to: ${setValue}`);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.platform.log.error(`Error setting On state: ${error.message}`);
+      } else {
+        this.platform.log.error('An unknown error occurred while setting On state');
+      }
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+  }
+
+  async handleOnGetSpeed(): Promise<CharacteristicValue> {
+    try {
+      await this.ensureConnection();
+      const response = await this.client.readInputRegisters(this.platform.speedRegister, 1);
+      const currentValue = response.data[0];
+      this.platform.log.debug('Speed is:', currentValue);
+      return currentValue;
+    } catch (error) {
+      if (error instanceof Error) {
+        this.platform.log.error(`Error getting speed state: ${error.message}`);
+      } else {
+        this.platform.log.error('An unknown error occurred while getting speed state');
+      }
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+  }
+
+  async handleOnSetSpeed(value: CharacteristicValue) {
+    try {
+      await this.ensureConnection();
+      const response = await this.client.readInputRegisters(this.platform.regimeRegister, 1);
       const state = response.data[0];
       if (state === 0) {
-        this.handleOnSetOn(1);
-        this.delay(5000);
+        await this.handleOnSetOn(1);
+        await this.delay(5000);
       }
-      await client.writeRegisters(this.platform.speedRegister, [value]);
-    }catch (error) {
-      console.error(error);
+      await this.client.writeRegisters(this.platform.speedRegister, [value as number]);
+      this.platform.log.debug(`Set Speed to: ${value}`);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.platform.log.error(`Error setting speed state: ${error.message}`);
+      } else {
+        this.platform.log.error('An unknown error occurred while setting speed state');
+      }
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
   }
 
-  connectModbusClient() {
-    client.connectTCP(this.platform.ip, { port: this.platform.port })
-      .then(() => {
-        this.platform.log.debug(`Připojeno k Modbus zařízení na adrese ${this.platform.ip}:${this.platform.port}`);
-      })
-      .catch((error) => {
-        this.platform.log.debug(`Chyba při připojení k Modbus zařízení: ${error.message}`);
-      });
-  }
-
-  delay(ms: number): Promise<void> {
+  private delay(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
 }
